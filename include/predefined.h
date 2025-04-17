6d3d7556db45ec3ce1969f1293899a48ad566be7
metrovian <paramedoct@gmail.com>
2025-04-17 13:36:23 +0000

    오디오 설정 파라미터 업데이트

--- include/predefined.h
+++ include/predefined.h
@@ -1,6 +1,17 @@
 #pragma once
 #include <stdio.h>
 
+#define AUD_DEVICE_CAPTURE	"default"
+#define AUD_DEVICE_PLAYBACK	"default"
+
+#define AUD_CHANNELS		1
+#define AUD_SAMPLE_RATE		44100
+#define AUD_BUFFER_FRAMES	44100
+
+#define AUD_FORMAT		SND_PCM_FORMAT_S16_LE
+#define AUD_MODE_CAPTURE	SND_PCM_STREAM_CAPTURE
+#define AUD_MODE_PLAYBACK	SND_PCM_STREAM_PLAYBACK
+
 #define WARN "\033[31m"
 #define INFO "\033[32m"
 #define RESET "\033[0m"
