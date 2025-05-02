5373fabef11934d21bfd4390f34c68dd682b7e86
metrovian <paramedoct@gmail.com>
2025-05-02 13:05:23 +0000

    오디오 스트림 DASH 오타 업데이트

--- include/stream_dash.h
+++ include/stream_dash.h
@@ -4,5 +4,5 @@
 
 extern int8_t stream_dash_open(FILE **stream, const char *path);
 extern int8_t stream_dash_close(FILE **stream);
-extern int8_t stream_dash_transmission_payloads(FILE **stream, int8_t *coptr, int32_t *pakcet_payloads);
+extern int8_t stream_dash_transmission_payloads(FILE **stream, int8_t *coptr, int32_t *packet_payloads);
 
