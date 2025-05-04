dbb43e99f3a465aa2f8a910e01f76a3ff519fa9a
metrovian <paramedoct@gmail.com>
2025-05-04 03:34:32 +0000

    오디오 스트림 JANUS 관리자 생성

---
+++ source/stream_janus.c
@@ -0,0 +1,52 @@
+#include "stream_janus.h"
+#include "predefined.h"
+
+extern int8_t stream_janus_open(FILE **stream, const char *path)
+{
+	char stream_command[512];
+	
+	snprintf(
+	stream_command, 
+	sizeof(stream_command),
+	"sudo "
+	"gst-launch-1.0"
+	"fdsrc ! "
+	"queue ! "
+	"identity sync=true ! "
+	"opusparse ! "
+	"rtpopuspay ! "
+	"udpsink host=%s port=5555",
+	path);
+
+	*stream = popen(stream_command, "w");
+
+	if(!*stream)
+	{
+		DBG_WARN("failed to open janus stream");
+		return -1;
+	}
+
+	DBG_INFO("janus stream open success");
+	return 0;
+}
+
+extern int8_t stream_janus_close(FILE **stream)
+{
+	pclose(*stream);
+
+	DBG_INFO("janus stream close success");
+	return 0;
+}
+
+extern int8_t stream_janus_transmission_payloads(FILE **stream, int8_t *coptr, int32_t *packet_payloads)
+{
+	if (fwrite(coptr, *packet_payloads, 1, *stream) != 1)
+	{
+		DBG_WARN("failed to stream payloads");
+		return -1;
+	}
+
+	fflush(*stream);
+
+	return 0;
+}
