257610a2e9360f1abbce086c5bb301d1e47f4bd7
metrovian <paramedoct@gmail.com>
2025-05-15 20:33:05 +0900

    오디오 녹음 관련 소비자 프로세스 제거

--- include/thread_consumer.h
+++ include/thread_consumer.h
@@ -12,17 +12,15 @@ typedef enum
 {
 	CONSUMER_NONE = 0,
 	CONSUMER_PLAYBACK = 1,
-	CONSUMER_RECORD = 2,
-	CONSUMER_TRANSMISSION_HLS = 3,
-	CONSUMER_TRANSMISSION_DASH = 4,
-	CONSUMER_TRANSMISSION_JANUS = 5,
+	CONSUMER_TRANSMISSION_HLS = 2,
+	CONSUMER_TRANSMISSION_DASH = 3,
+	CONSUMER_TRANSMISSION_JANUS = 4,
 } thread_consumer;
 
 extern audio_device *g_audio_playback;
 extern thread_consumer g_thread_consumer;
 
 extern void *thread_consumer_playback(void *argument);
-extern void *thread_consumer_record(void *argument);
 extern void *thread_consumer_transmission_hls(void *argument);
 extern void *thread_consumer_transmission_dash(void *argument);
 extern void *thread_consumer_transmission_janus(void *argument);
