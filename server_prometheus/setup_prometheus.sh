3216e0951abc942f6e6026a92cee59f70762174e
metrovian <paramedoct@gmail.com>
2025-05-24 22:02:36 +0900

    시계열 데이터베이스 설정 스크립트 업데이트

--- server_prometheus/setup_prometheus.sh
+++ server_prometheus/setup_prometheus.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo cp -arv prometheus/prometheus-pushgateway /etc/default/
 sudo cp -arv prometheus/prometheus.yml /etc/prometheus/
+sudo cp -arv prometheus/prometheus-pushgateway /etc/default/
+
 sudo systemctl reload prometheus
+sudo systemctl reload prometheus-pushgateway
