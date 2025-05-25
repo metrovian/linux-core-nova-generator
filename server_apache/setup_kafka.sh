a9f895d06e0d8ea4141f92785057ae5531615caa
metrovian <paramedoct@gmail.com>
2025-05-25 19:44:18 +0900

    메세지 브로커 설정 스크립트 부트스트랩 자동화

--- server_apache/setup_kafka.sh
+++ server_apache/setup_kafka.sh
@@ -2,6 +2,10 @@
 
 IP_ADDR=$(hostname -I | awk '{print $1}')
 NEW_LINE="advertised.listeners=PLAINTEXT://${IP_ADDR}:9092,LOCAL://localhost:9094"
+NEW_META=/opt/kafka/bin/kafka-storage.sh
 
 sudo sed -i "s|^advertised.listeners=.*|${NEW_LINE}|" "kafka/server.properties"
 sudo cp -arv kafka/server.properties /opt/kafka/config/server.properties
+
+sudo rm -rf /tmp/kraft-combined-logs/
+sudo ${NEW_META} format -t "$(${NET_META} random-uuid)" -c /opt/kafka/config/server.properties
