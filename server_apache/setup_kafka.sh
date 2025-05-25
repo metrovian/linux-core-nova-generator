b7577cc85a0d63050ede38ad214421025a99e0c4
metrovian <paramedoct@gmail.com>
2025-05-25 19:57:29 +0900

    메세지 브로커 UUID 설정 업데이트

--- server_apache/setup_kafka.sh
+++ server_apache/setup_kafka.sh
@@ -3,9 +3,10 @@
 IP_ADDR=$(hostname -I | awk '{print $1}')
 NEW_LINE="advertised.listeners=PLAINTEXT://${IP_ADDR}:9092,LOCAL://localhost:9094"
 NEW_META=/opt/kafka/bin/kafka-storage.sh
+NEW_UUID=$(${NET_META} random-uuid)
 
 sudo sed -i "s|^advertised.listeners=.*|${NEW_LINE}|" "kafka/server.properties"
 sudo cp -arv kafka/server.properties /opt/kafka/config/server.properties
 
 sudo rm -rf /tmp/kraft-combined-logs/
-sudo ${NEW_META} format -t "$(${NET_META} random-uuid)" -c /opt/kafka/config/server.properties
+sudo ${NEW_META} format -t ${NEW_UUID} -c /opt/kafka/config/server.properties
