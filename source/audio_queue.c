37568e0e5f38603d317f195b738f3334c7f4ebdd
metrovian <paramedoct@gmail.com>
2025-05-29 20:32:01 +0900

    오디오 버퍼 자료구조 로그 업데이트

--- source/audio_queue.c
+++ source/audio_queue.c
@@ -1,19 +1,19 @@
 #include "audio_queue.h"
 #include "thread_monitor.h"
+#include "wrapper_spdlog.h"
 #include "predefined.h"
 
 extern audio_queue *audio_queue_create(int32_t aucap) {
 	audio_queue *queue = (audio_queue *)malloc(sizeof(audio_queue));
 	if (!queue) {
-		DBG_WARN("failed to allocate audio queue");
+		log_error("failed to allocate audio queue structure");
 		return NULL;
 	}
 
 	queue->data = (int16_t *)malloc(sizeof(int16_t) * aucap);
 	if (!queue->data) {
 		free(queue);
-
-		DBG_WARN("failed to allocate audio queue data");
+		log_error("failed to allocate audio queue data");
 		return NULL;
 	}
 
@@ -24,25 +24,27 @@ extern audio_queue *audio_queue_create(int32_t aucap) {
 	pthread_mutex_init(&queue->mutex, NULL);
 	pthread_cond_init(&queue->push_available, NULL);
 	pthread_cond_init(&queue->pop_available, NULL);
+	log_debug("audio queue create success");
 	return queue;
 }
 
 extern void audio_queue_destroy(audio_queue *auque) {
-	if (!auque) {
-		return;
+	if (auque) {
+		pthread_mutex_destroy(&auque->mutex);
+		pthread_cond_destroy(&auque->push_available);
+		pthread_cond_destroy(&auque->pop_available);
+		free(auque->data);
+		free(auque);
+		log_debug("audio queue destroy success");
 	}
 
-	pthread_mutex_destroy(&auque->mutex);
-	pthread_cond_destroy(&auque->push_available);
-	pthread_cond_destroy(&auque->pop_available);
-	free(auque->data);
-	free(auque);
 	return;
 }
 
 extern void audio_queue_push(audio_queue *auque, int16_t *auptr, int32_t *push_samples) {
 	pthread_mutex_lock(&auque->mutex);
 	while (auque->capacity < auque->size + *push_samples) {
+		log_warn("failed to push audio frames immediately");
 		pthread_cond_wait(&auque->push_available, &auque->mutex);
 	}
 
@@ -60,6 +62,7 @@ extern void audio_queue_push(audio_queue *auque, int16_t *auptr, int32_t *push_s
 extern void audio_queue_pop(audio_queue *auque, int16_t *auptr, int32_t *pop_samples) {
 	pthread_mutex_lock(&auque->mutex);
 	while (auque->size < *pop_samples) {
+		log_warn("failed to pop audio frames immediately");
 		pthread_cond_wait(&auque->pop_available, &auque->mutex);
 	}
 
