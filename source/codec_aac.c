d6419964e7fdbe4862804641ee6f865d16a6bdf8
metrovian <paramedoct@gmail.com>
2025-05-29 22:17:56 +0900

    오디오 코덱 관리자 로그 업데이트

--- source/codec_aac.c
+++ source/codec_aac.c
@@ -1,56 +1,57 @@
 #include "codec_aac.h"
+#include "wrapper_spdlog.h"
 #include "predefined.h"
 
 extern int8_t codec_aac_open(codec_aac *aucod, int16_t channels, int32_t sample_rate) {
 	if (aacEncOpen(&aucod->encoder, 0, 0) > 0) {
-		DBG_WARN("failed to open encoder");
+		log_error("failed to open aac encoder");
 		return -1;
 	}
 
 	if (aacEncoder_SetParam(aucod->encoder, AACENC_AOT, AAC_MODE) > 0) {
-		DBG_WARN("failed to set mode");
+		log_error("failed to set aac encoder mode");
 		return -1;
 	}
 
 	if (aacEncoder_SetParam(aucod->encoder, AACENC_CHANNELMODE, channels) > 0) {
-		DBG_WARN("failed to set channels");
+		log_error("failed to set aac encoder channels");
 		return -1;
 	}
 
 	if (aacEncoder_SetParam(aucod->encoder, AACENC_SAMPLERATE, sample_rate) > 0) {
-		DBG_WARN("failed to set sample rate");
+		log_error("failed to set aac encoder sample rate");
 		return -1;
 	}
 
 	if (aacEncoder_SetParam(aucod->encoder, AACENC_BITRATE, AAC_BIT_RATE) > 0) {
-		DBG_WARN("failed to set bit rate");
+		log_error("failed to set aac encoder bit rate");
 		return -1;
 	}
 
 	if (aacEncoder_SetParam(aucod->encoder, AACENC_TRANSMUX, AAC_TRANSMUX) > 0) {
-		DBG_WARN("failed to set transport multiplexing");
+		log_error("failed to set aac encoder transport multiplexing");
 		return -1;
 	}
 
 	if (aacEncEncode(aucod->encoder, NULL, NULL, NULL, NULL) > 0) {
-		DBG_WARN("failed to prepare encoder");
+		log_error("failed to prepare aac encoder");
 		return -1;
 	}
 
 	aucod->decoder = aacDecoder_Open(AAC_TRANSMUX, 1);
 	if (!aucod->decoder) {
-		DBG_WARN("failed to open decoder");
+		log_error("failed to open aac decoder");
 		return -1;
 	}
 
-	DBG_INFO("aac codec open success");
+	log_debug("aac codec open success");
 	return 0;
 }
 
 extern int8_t codec_aac_close(codec_aac *aucod) {
 	aacEncClose(&aucod->encoder);
 	aacDecoder_Close(aucod->decoder);
-	DBG_INFO("aac codec close success");
+	log_debug("aac codec close success");
 	return 0;
 }
 
@@ -79,7 +80,7 @@ extern int8_t codec_aac_encode(codec_aac *aucod, int16_t *auptr, int8_t *coptr, 
 	aac_out.bufSizes = &outarg_size;
 	aac_out.bufElSizes = &outarg_el;
 	if (aacEncEncode(aucod->encoder, &aac_in, &aac_out, &aac_inargs, &aac_outargs) > 0) {
-		DBG_WARN("failed to encode frames");
+		log_error("failed to encode aac payloads");
 		return -1;
 	}
 
@@ -91,12 +92,12 @@ extern int8_t codec_aac_decode(codec_aac *aucod, int16_t *auptr, int8_t *coptr, 
 	int8_t *enptr = coptr;
 	int32_t *envalid = packet_payloads;
 	if (aacDecoder_Fill(aucod->decoder, &enptr, packet_payloads, envalid) > 0) {
-		DBG_WARN("failed to fill payloads");
+		log_error("failed to fill aac payloads");
 		return -1;
 	}
 
 	if (aacDecoder_DecodeFrame(aucod->decoder, auptr, AUD_BUFFER_FRAMES * AUD_CHANNELS, 0) > 0) {
-		DBG_WARN("failed to decode payloads");
+		log_error("failed to decode aac payloads");
 		return -1;
 	}
 
