f39aa9c8664dec17e0b03e43def20441a42383d1
metrovian <paramedoct@gmail.com>
2025-06-02 21:29:08 +0900

    아파치 서비스 버전 업데이트

--- service-apache/setup.sh
+++ service-apache/setup.sh
@@ -4,13 +4,10 @@ sudo apt update
 sudo apt install openjdk-11-jdk wget -y
 sudo apt install zookeeperd
 
-sudo wget https://downloads.apache.org/kafka/3.6.1/kafka_2.13-3.6.1.tgz
-sudo tar -xvzf kafka_2.13-3.6.1.tgz
-sudo rm -rf kafka_2.13-3.6.1.tgz 
-sudo mv kafka_2.13-3.6.1 /opt/kafka
-
-sudo useradd -d /opt/kafka -s /bin/bash kafka
-sudo chown -R kafka:kafka /opt/kafka
+sudo wget https://dlcdn.apache.org/kafka/4.0.0/kafka_2.13-4.0.0.tgz
+sudo tar -xvzf kafka_2.13-4.0.0.tgz
+sudo rm -rf kafka_2.13-4.0.0.tgz
+sudo mv kafka_2.13-4.0.0 /opt/kafka
 sudo cp -arv kafka/kafka.service /etc/systemd/system/kafka.service
 
 sudo systemctl daemon-reexec
@@ -18,5 +15,5 @@ sudo systemctl daemon-reload
 sudo systemctl enable kafka
 sudo systemctl start kafka
 
-./setup-kafka
-./setup-zookeeper
+./setup-kafka.sh
+./setup-zookeeper.sh
