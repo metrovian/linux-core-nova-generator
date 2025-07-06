c896dcd8fa3caea8e41f009ccfeb83be3098e99f
metrovian <paramedoct@gmail.com>
2025-07-06 14:57:00 +0900

    서비스 의존성 인스톨 스크립트 업데이트

---
+++ service-meetecho/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install pkg-config libtool automake
+
+sudo apt install \
+        libmicrohttpd-dev \
+        libjansson-dev \
+	libssl-dev \
+        libsofia-sip-ua-dev \
+        libglib2.0-dev \
+	libopus-dev \
+        libogg-dev \
+        libcurl4-openssl-dev \
+        liblua5.3-dev \
+	libconfig-dev \
