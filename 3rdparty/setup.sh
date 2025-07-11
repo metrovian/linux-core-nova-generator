6705c25608934c419c267e97df8814e926af42f2
metrovian <paramedoct@gmail.com>
2025-07-11 21:54:22 +0900

    서드파티 업데이트

--- 3rdparty/setup.sh
+++ 3rdparty/setup.sh
@@ -6,6 +6,7 @@ sudo apt install ifstat ffmpeg
 sudo apt install \
         libasound2-dev \
         libcurl4-openssl-dev \
+        libcxxopts-dev \
         libfdk-aac-dev \
         libmicrohttpd-dev \
         libopus-dev \
