0728093ddbbffee7278db92fce39210db35dfccb
metrovian <paramedoct@gmail.com>
2025-07-11 22:52:58 +0900

    메인 파라미터 업데이트

--- module/main_hls.c
+++ module/main_hls.c
@@ -1,6 +1,7 @@
 #include "thread_producer.h"
 #include "thread_consumer.h"
 #include "thread_monitor.h"
+#include "wrapper_cxxopts.h"
 #include "wrapper_spdlog.h"
 #include "predefined.h"
 
@@ -13,31 +14,21 @@ void HANDLE_SIGINT(int32_t signal) {
 }
 
 int32_t main(int32_t argc, char *argv[]) {
+	main_arguments arguments;
 	signal(SIGINT, HANDLE_SIGINT);
-	pthread_t pthread_producer;
-	pthread_t pthread_consumer;
-	char path_hls[256];
-	char path_zk[256];
-	char path_kf[256];
-	if (argc == 1 || argc == 2) {
-		pthread_create(&pthread_producer, NULL, thread_producer_aac, NULL);
-		strncpy(path_hls, "/var/www/hls", sizeof(path_hls));
-		strncpy(path_zk, "", sizeof(path_zk));
-		strncpy(path_kf, "", sizeof(path_kf));
-		if (argc == 2) {
-			snprintf(path_zk, sizeof(path_zk), "%s:%d", argv[1], NET_ZOOKEEPER_PORT);
-			snprintf(path_kf, sizeof(path_kf), "%s:%d", argv[1], NET_KAFKA_PORT);
-		}
-	} else {
+	if (parse_arguments_hls(&arguments, argc, argv) != 0) {
 		log_critical("invalid parameters");
 		return -1;
 	}
 
+	pthread_t pthread_producer;
+	pthread_t pthread_consumer;
+	pthread_create(&pthread_producer, NULL, thread_producer_aac, NULL);
 	usleep(AUD_BUFFER_TIMES);
-	pthread_create(&pthread_consumer, NULL, thread_consumer_transmission_hls, path_hls);
-	thread_monitor_resource_ramdisk(path_hls);
-	thread_monitor_zookeeper_manager(path_zk);
-	thread_monitor_kafka_manager(path_kf);
+	pthread_create(&pthread_consumer, NULL, thread_consumer_transmission_hls, arguments.path_segments);
+	thread_monitor_resource_ramdisk(arguments.path_segments);
+	thread_monitor_zookeeper_manager(arguments.path_zookeeper);
+	thread_monitor_kafka_manager(arguments.path_kafka);
 	usleep(AUD_BUFFER_TIMES);
 	thread_monitor_start();
 	pthread_join(pthread_producer, NULL);
