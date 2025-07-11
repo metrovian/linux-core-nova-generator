0839d6ad89f292255673d9765ca5e752dd14a63c
metrovian <paramedoct@gmail.com>
2025-07-11 22:54:14 +0900

    서드파티 빌드 스크립트 업데이트

--- service-apache/setup.sh
+++ service-apache/setup.sh
@@ -6,8 +6,9 @@ sudo apt install zookeeperd
 
 sudo wget https://dlcdn.apache.org/kafka/4.0.0/kafka_2.13-4.0.0.tgz
 sudo tar -xvzf kafka_2.13-4.0.0.tgz
-sudo rm -rf kafka_2.13-4.0.0.tgz
-sudo mv kafka_2.13-4.0.0 /opt/kafka
+sudo rm -rvf kafka_2.13-4.0.0.tgz
+sudo mv -vf kafka_2.13-4.0.0 /opt/kafka
+sudo rm -rvf kafka_2.13-4.0.0
 sudo cp -arv kafka/kafka.service /etc/systemd/system/kafka.service
 
 sudo systemctl daemon-reexec
